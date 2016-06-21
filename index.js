616e4e1d341737e440271fd4c6367a9ab31cc782
LeoPelan <leo.pelan@gmail.com>
2016-06-21 14:19:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const fs = require('fs');
+const screenshot = require('screenshot-stream');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/screen', function(req, res){
+  var site = req.query.url;
+  const stream = screenshot(site, '1024x768', {crop: true});
+
+stream.pipe(fs.createWriteStream('test.png'));
+res.sendStatus(200)
+});
+
+io.on('connection', function(socket){
+  console.log('connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
